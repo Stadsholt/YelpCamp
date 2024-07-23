32981a40d3199d465a57aac5bd28db81966be3fa
Sebastian <stadsholt@users.noreply.github.com>
2024-07-23 16:24:30 +0200

    Styling

--- seeds/index.js
+++ seeds/index.js
@@ -20,9 +20,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: `https://picsum.photos/400?random=${Math.random()}`,
+            description:  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit rem vero autem ullam deleniti natus porro aliquam cupiditate, ratione ab consectetur vel nam hic nulla velit earum explicabo, quos possimus!',
+            price 
         })
         await camp.save();
     }
