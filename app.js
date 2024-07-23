f27565ed0781c287b69200286eaa78f82552f626
Sebastian <stadsholt@users.noreply.github.com>
2024-07-23 14:38:30 +0200

    Update campgrounds

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require ('express');
 const path = require('path');
 const mongoose = require ('mongoose');
 const Campground = require('./models/campground');
+const methodOverride = require('method-override');
+
+
+
+
+
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
 });
@@ -20,8 +26,8 @@ app.set('view engine','ejs');
 app.set('views', path.join(__dirname,'views'));
 
 
-app.use(express.urlencoded({extended:true}))
-
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
 
 
 app.get('/', (req, res) => {
@@ -52,6 +58,18 @@ app.get('/campgrounds/:id', async (req, res,) => {
     res.render('campgrounds/show', { campground });
 });
 
+
+app.get('/campgrounds/:id/edit', async (req, res,) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/edit', { campground });
+
+});
+
+app.put('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    res.redirect(`/campgrounds/${campground._id}`)
+});
 
 
 
