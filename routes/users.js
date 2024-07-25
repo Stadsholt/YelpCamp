f1276e8c52a61f5e13845072f43595b0ecb6d65e
Sebastian <stadsholt@users.noreply.github.com>
2024-07-25 19:35:30 +0200

    removed seeds

--- routes/users.js
+++ routes/users.js
@@ -35,11 +35,13 @@ router.post('/login', passport.authenticate('local', { failureFlash: true, failu
     res.redirect(redirectUrl);
 })
 
-router.get('/logout', (req, res) => {
-    req.logout();
-    req.flash('success', "Goodbye!");
-    res.redirect('/campgrounds');
-})
+router.get('/logout', (req, res, next) => {
+    req.logout(function(err) {
+        if (err) { return next(err); }
+        req.flash('success', "Goodbye!");
+        res.redirect('/campgrounds');
+    });
+});
 
 module.exports = router;
 
--- seeds/index.js
+++ seeds/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const cities = require('./cities');
-const {places, descriptors} = require('./seedHelpers');
+const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
@@ -16,21 +16,7 @@ db.once("open", () => {
 const sample = array => array[Math.floor(Math.random() * array.length)];
 
 
-const seedDB = async () => {
-    await Campground.deleteMany({});
-    for (let i = 0; i < 50; i++) {
-        const random1000 = Math.floor(Math.random() * 1000);
-        const price = Math.floor(Math.random() * 20) + 10;
-        const camp = new Campground({
-            location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`,
-            image: `https://picsum.photos/400?random=${Math.random()}`,
-            description:  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit rem vero autem ullam deleniti natus porro aliquam cupiditate, ratione ab consectetur vel nam hic nulla velit earum explicabo, quos possimus!',
-            price 
-        })
-        await camp.save();
-    }
-}
+
 
 seedDB().then(() => {
     mongoose.connection.close();
