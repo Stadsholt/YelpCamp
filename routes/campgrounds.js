e226e7efd4802066975f5812560c55aa1e9348f5
Sebastian <stadsholt@users.noreply.github.com>
2024-07-25 18:21:08 +0200

    author

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -31,6 +31,7 @@ router.get('/new', isLoggedIn, (req, res) => {  // uses isLoggedIn middleware to
 
 router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, next) => {
     const campground = new Campground(req.body.campground);
+    campground.author = req.user._id; // find user id so we can add it to new campground
     await campground.save();
     req.flash('succes', 'Successfully made a new campground!');
     res.redirect(`/campgrounds/${campground._id}`)
@@ -38,7 +39,7 @@ router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, nex
 
 
 router.get('/:id', catchAsync(async (req, res,) => {
-    const campground = await Campground.findById(req.params.id).populate('reviews');
+    const campground = await Campground.findById(req.params.id).populate('reviews').populate('author');
     if(!campground)
     {
         req.flash('error','Cannot find that campground!')
