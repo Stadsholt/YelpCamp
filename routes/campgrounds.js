c2fc6c066914a79385f821b75962a053eafac9be
Sebastian <stadsholt@users.noreply.github.com>
2024-07-26 00:23:13 +0200

    Upload multiple images

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,11 +4,24 @@ const campgrounds = require('../controllers/campgrounds');
 const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 
+const multer = require('multer')
+const upload = multer({dest:'uploads/'})
+
+
 const Campground = require('../models/campground');
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
+    //.post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
+
+    //.post(upload.array('image'), (req,res)=>{
+    //    res.send(req.body, req.files)
+    //})
+
+    .post(upload.array('image'), (req, res) => {
+        console.log(req.body,req.files);
+        res.status(200).send({ body: req.body, file: req.files });
+    });
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm)
 
