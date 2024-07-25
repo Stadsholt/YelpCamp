3055464243fb814c9de7650203aea8455c37bfc0
Sebastian <stadsholt@users.noreply.github.com>
2024-07-25 19:52:08 +0200

    Refactoring and stars

--- routes/users.js
+++ routes/users.js
@@ -3,48 +3,16 @@ const router = express.Router();
 const passport = require('passport');
 const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
+const users = require('../controllers/users');
 
-router.get('/register', (req, res) => {   // render register page
-    res.render('users/register');
-});
+router.route('/register')
+    .get(users.renderRegister)
+    .post(catchAsync(users.register));
 
-router.post('/register', catchAsync(async (req, res, next) => {  // try/catch to get errors, flash error. 
-    try {
-        const { email, username, password } = req.body;
-        const user = new User({ email, username });
-        const registeredUser = await User.register(user, password);
-        req.login(registeredUser, err => {   // logs user in when they register
-            if (err) return next(err);
-            req.flash('success', 'Welcome to Yelp Camp!');
-            res.redirect('/campgrounds');
-        })
-    } catch (e) {
-        req.flash('error', e.message);
-        res.redirect('register');
-    }
-}));
+router.route('/login')
+    .get(users.renderLogin)
+    .post(passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login)
 
-router.get('/login', (req, res) => {    // render login page
-    res.render('users/login');
-})
-
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {   // magic "authenticate" passport middleware. + storereturnto middleware
-    req.flash('success', 'Welcome back!');
-    const redirectUrl = req.session.returnTo || '/campgrounds';
-    delete req.session.returnTo;
-    res.redirect(redirectUrl);
-})
-
-router.get('/logout', (req, res, next) => {
-    req.logout(function(err) {
-        if (err) { return next(err); }
-        req.flash('success', "Logged out");
-        res.redirect('/campgrounds');
-    });
-});
+router.get('/logout', users.logout)
 
 module.exports = router;
-
-
-
-
