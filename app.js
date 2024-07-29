de42eaea0ab287fdec8ed5729217c535c03df633
Sebastian <stadsholt@users.noreply.github.com>
2024-07-29 19:44:19 +0200

    Deployment

--- app.js
+++ app.js
@@ -2,40 +2,32 @@ if (process.env.NODE_ENV !== "production") {
     require('dotenv').config();
 }
 
-console.log(process.env.SECRET)
-
-const express = require ('express');
+const express = require('express');
 const path = require('path');
-const mongoose = require ('mongoose');
+const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
-const Joi = require('joi')
-
-const flash = require('connect-flash');
-
-const ExpressError = require('./utils/ExpressError');
 const session = require('express-session');
+const flash = require('connect-flash');
+const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 const helmet = require('helmet');
-
 const mongoSanitize = require('express-mongo-sanitize');
-
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 
+const MongoDBStore = require("connect-mongo")(session);
 
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
-    useNewUrlParser:true,
-    UseUnifiedTopology:true,
+mongoose.connect(dbUrl, {
 });
 
-
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
+db.on("error", console.error.bind(console, "Connection error:"));
 db.once("open", () => {
     console.log("Database connected");
 });
@@ -43,28 +35,44 @@ db.once("open", () => {
 const app = express();
 
 app.engine('ejs', ejsMate)
-app.set('view engine','ejs');
-app.set('views', path.join(__dirname,'views'));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
 
-
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(mongoSanitize({
     replaceWith: '_'
 }))
-const sessionConfig={
+const secret = process.env.SECRET || 'thisshouldbeabettersecret!';
+
+const store = new MongoDBStore({
+    url: dbUrl,
+    secret,
+    touchAfter: 24 * 60 * 60
+});
+
+store.on("error", function (e) {
+    console.log("Session store error", e)
+})
+
+
+const sessionConfig = {
+    store,
     name:'VIEWENGINE',  // Give randomname, so others have harder time finding it
-    secret: 'Supa',
-    resave:false,
-    saveUninitialized:true,
-    cookie:{
-        httpOnly:true,
-        expires: Date.now()+1000*60*60*24*7,
-        maxAge: 1000*60*60*24*7,
-    },
-   
+    secret,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        // secure: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
 }
+
+
+
 app.use(session(sessionConfig))
 app.use(flash());
 app.use(helmet());
